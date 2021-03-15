6a7446f901fe7984c8caf94b860ce61d248a8bc8
creatorQuen <spastime@yandex.ru>
2021-03-15 21:16:28 +0300

    Add project files.

---
+++ MyProject1/ArrayList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyProject1
+{
+    public class ArrayList
+    {
+        public int Length { get; private set; }
+
+        private int[] _array;
+
+        public ArrayList()
+        {
+            Length = 0;
+
+            _array = new int[10];
+        }
+
+        public void Add(int value)
+        {
+            if(Length == _array.Length)
+            {
+                UpSize();
+            }
+
+            _array[Length] = value;
+            Length++;
+        }
+
+        private void UpSize()
+        {
+            int newLenght = (int)(_array.Length * 1.33d + 1);
+
+            int[] tmpArray = new int[newLenght];
+
+            for(int i=0; i < _array.Length; i++)
+            {
+                tmpArray[i] = _array[i];
+            }
+
+            _array = tmpArray;
+
+        }
+
+        
+    }
+}
